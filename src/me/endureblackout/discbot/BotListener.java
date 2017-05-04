badf8d74aeac8cf5471e93b77d042215fabf0eda
EndureBlackout <jacobellingson@aol.com>
2017-05-04 08:19:00 -0500

    Implementing meme generator API. Not tested yet.

--- src/me/endureblackout/discbot/BotListener.java
+++ src/me/endureblackout/discbot/BotListener.java
@@ -1,6 +1,17 @@
 package me.endureblackout.discbot;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import net.dv8tion.jda.core.Permission;
 import net.dv8tion.jda.core.entities.Emote;
@@ -90,7 +101,22 @@ public class BotListener extends ListenerAdapter {
 		if(e.getMessage().getRawContent().equalsIgnoreCase("/meme")) {
 			e.getMessage().delete().queue();
 			
-			e.getChannel().sendMessage("").queue();
+			JSONObject ob = null;
+			JSONArray ar = null;
+			
+			try {
+				ob = readJsonFromUrl("http://api.giphy.com/v1/gifs/search?q=meme&api_key=dc6zaTOxFJmzC");
+			} catch (Exception e1) {
+				e1.printStackTrace();
+			}
+			
+			try {
+				ar = (JSONArray) ob.getJSONArray("data");
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+			
+			e.getChannel().sendMessage(ob.toString()).queue();
 		}
 	}
 	
@@ -99,8 +125,52 @@ public class BotListener extends ListenerAdapter {
 		Emote emote = e.getEmote();
 		
 		e.getGuild().getPublicChannel().sendMessage("New emote added! " + emote);
-		TextChannel channel = e.getGuild().getTextChannelById("Announcements");
+		List<TextChannel> channels = e.getGuild().getTextChannels();
 		
-		channel.sendMessage("New emote added! " + emote);
+		for(TextChannel announce : channels) {
+			if(announce.getName().equalsIgnoreCase("announcements")) {
+				announce.sendMessage("Emoji added!").queue();
+				announce.sendMessage("Emoji: " + emote.getName()).queue();
+			}
+		}
+	}
+	
+	public static String readAll(Reader rd) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		int cp;
+		
+		while((cp = rd.read()) != -1) {
+			sb.append((char) cp);
+		}
+		
+		return sb.toString();
+	}
+	
+	public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+		InputStream is = new URL(url).openStream();
+		
+		try {
+			BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+			String jsonText = readAll(rd);
+			JSONObject json = new JSONObject(jsonText);
+			
+			return json;
+		} finally {
+			is.close();
+		}
+	}
+	
+	public static JSONArray readArray(String url) throws IOException, JSONException {
+		InputStream is = new URL(url).openStream();
+		
+		try {
+			BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+			String jsonText = readAll(rd);
+			JSONArray json = new JSONArray(jsonText);
+			
+			return json;
+		} finally {
+			is.close();
+		}
 	}
 }
